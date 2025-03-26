0c5aeac028575c93e37f5765c9ca541092f45705
dscp46 <celestine+dev@gramaize.eu>
2025-03-26 07:28:33 +0100

    Add documentation

--- src/ringbuffer.c
+++ src/ringbuffer.c
@@ -1,6 +1,13 @@
 #include "ringbuffer.h"
 
-
+/** 
+ * @brief Create a ring buffer
+ * 
+ * @param[in] size 	The internal buffer size (size_t)
+ *
+ * @return 	A pointer to the allocated ring buffer (ringbuffer_t*)
+ * @retval NULL		Memory allocation failure..
+ */
 ringbuffer_t* ringbuffer_alloc( size_t size)
 {
 	ringbuffer_t *buf = (ringbuffer_t*) malloc(sizeof(ringbuffer_t));
@@ -19,9 +26,13 @@ ringbuffer_t* ringbuffer_alloc( size_t size)
 	buf->cur_write = buf->buffer;
 	
 	return buf;
-}
+}	
 
-
+/** 
+ * @brief Frees up the resources associated to a ring buffer
+ *
+ * @param[in] rbuf	The ringbuffer to free (ringbuffer_t*)
+ */
 void ringbuffer_free( ringbuffer_t *rbuf)
 {
 	if( rbuf == NULL )
@@ -34,8 +45,11 @@ void ringbuffer_free( ringbuffer_t *rbuf)
 	free( rbuf);
 }
 
-/** ringbuffer_bytes_available() -- Get how many bytes can be written at the moment
+/** 
+ * @brief Get how many bytes can be written at the moment
  *
+ * @param[in] buf	The ringbuffer we want to check (ringbuffer_t*) 
+ * @return 	Number of available bytes to be written, at a given time.(size_t)
  */
 size_t ringbuffer_bytes_available( const ringbuffer_t *buf)
 {
@@ -45,8 +59,14 @@ size_t ringbuffer_bytes_available( const ringbuffer_t *buf)
 	return buf->size - used - 1;
 }
 
-/** ringbuffer_pull() -- Pull at most n bytes from the ring buffer
- *  This function
+/**
+ * @brief Pull at most n bytes from the ring buffer
+ *
+ * @param[in] dest	The destination buffer (void*)
+ * @param[in] n		Read at most n bytes (size_t)
+ * @param[in] buf	The ringbuffer we want to pull bytes from (rungbuffer_t*)
+ *
+ * @return	The actual number of pulled bytes (size_t)
  */
 size_t ringbuffer_pull( void *dest, size_t n, ringbuffer_t *buf)
 {
@@ -70,8 +90,13 @@ size_t ringbuffer_pull( void *dest, size_t n, ringbuffer_t *buf)
 	return n_read;
 }
 
-/** ringbuffer_push() -- Insert at most n bytes into ringbuffer
- * 
+/**
+ * @brief Insert at most n bytes into ringbuffer
+ * @param[in] src	Source buffer we want to insert data from (const void*)
+ * @param[in] n		Number of bytes we want to write to the ringbuffer (size_t)
+ * @param[in] buf	The ringbuffer we're writing to (ringbuffer_t*)
+ *
+ * @return	The number of actually written bytes (size_t)
  */
 size_t ringbuffer_push( const void *src, size_t n, ringbuffer_t *buf)
 {
