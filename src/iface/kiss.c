975d6b78bb1514794b41b4d071a4f81152a23d70
dscp46 <celestine+dev@gramaize.eu>
2025-04-17 02:21:01 +0200

    [iface_kiss] Initial implementation

--- src/iface/kiss.c
+++ src/iface/kiss.c
@@ -1,9 +1,12 @@
 
 #include "./kiss.h"
+#include "../dse.h"
 
+#include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /*** Internal definitions ***/
 
@@ -13,11 +16,33 @@
 // AX.25 frame: 1024b, Start & End markers: 2b, CRC32: 4b, Type: 1b 
 #define RX_MTU 1031
 
+// KISS protocol magic values
+#define FEND	0xC0U
+#define FESC	0xDBU
+#define TFEND	0xDCU
+#define TFESC	0xDDU
+
 typedef struct kiss_iface_attrs {
+
+	// Client socket file descriptor
 	int client_fd;
+	
+	// Remote client address
 	struct sockaddr* client_addr;
+	
+	// Remote client address size
 	size_t client_addr_sz;
+	
+	// Client handler thread
+	pthread_t handler_thread;
+	
+	// Parent dse
+	dse *sw;
 } kiss_iface_attrs;
+
+size_t kiss_encoded_size( const void* frame, size_t len);
+void kiss_encode( const void *in, size_t len, unsigned char type, void *out);
+int kiss_decode( const void *in, size_t len, unsigned char *type, void *out);
 
 void kiss_send( ax_iface *self, void *frame, size_t len);
 void *kiss_client_handler( void* arg);
@@ -42,11 +67,19 @@ ax_iface *kiss_ifup( int client_fd, struct sockaddr* client_addr, size_t client_
 	attrs->client_fd = client_fd;
 	attrs->client_addr = client_addr;
 	attrs->client_addr_sz = client_addr_sz;
+	attrs->sw = NULL;
 	iface->p = attrs;
 	
 	iface->send_frame = kiss_send;
 	
-	// TODO Start handler thread
+	// Start handler thread
+	if( pthread_create( &attrs->handler_thread, NULL, kiss_client_handler, iface) != 0) {
+		printf("Thread creation failed.\n");
+		kiss_ifdown( iface);
+		return NULL;
+	}
+	
+	// TODO: Set protocol state UP
 		
 	return iface;
 }
@@ -56,12 +89,15 @@ void kiss_ifdown( ax_iface *iface)
 	if ( iface == NULL )
 		return;
 	
-	// TODO Stop handler thread
-	
 	kiss_iface_attrs* attrs = (kiss_iface_attrs*) iface->p;
 	if ( attrs != NULL )
 	{
-		fclose( attrs->client_fd);
+		// Stop handler thread
+		pthread_cancel( attrs->handler_thread);
+		
+		// TODO: Set protocol state DOWN
+	
+		close( attrs->client_fd);
 		
 		if ( attrs->client_addr != NULL )
 			free( attrs->client_addr);
@@ -72,8 +108,127 @@ void kiss_ifdown( ax_iface *iface)
 	free( iface);
 }
 
+size_t kiss_encoded_size( const void* frame, size_t len)
+{
+	if ( frame == NULL )
+		return 0;
+
+	// Include head and tail FENDs, plus the data type
+	size_t result = 3;
+	unsigned char *src = (unsigned char*)frame;
+
+	while ( len-- > 0 )
+	{
+		if ( *src == FEND || *src == FESC )
+			++result;
+
+		++result;
+		++src;
+	}
+
+	return result;
+}
+
+void kiss_encode( const void *in, size_t len, unsigned char type, void *out)
+{
+	if ( in == NULL || out == NULL )
+		return;
+	
+	unsigned char *src = (unsigned char*)in;
+	unsigned char *dst = (unsigned char*)out;
+	
+	*dst++ = FEND;
+	*dst++ = type;
+	while( len-- > 0 )
+	{
+		switch ( (unsigned int)*src )
+		{
+		case FEND:
+			*dst++ = FESC;
+			*dst++ = TFEND;
+			++src;
+			break;
+
+		case FESC:
+			*dst++ = FESC;
+			*dst++ = TFESC;
+			++src;
+			break;
+
+		default:
+			*dst++ = *src++;
+		}
+	}
+	*dst = FEND;
+}
+
+int kiss_decode( const void *in, size_t len, unsigned char *type, void *out)
+{
+	if ( in == NULL || type == NULL || out == NULL )
+		return EINVAL;
+
+	unsigned char *src = (unsigned char *)in;
+	unsigned char *dst = (unsigned char*)out;
+	size_t n = len-3;
+	
+	if ( len <= 3 || *src++ != FEND )
+		return EBADMSG;
+
+	*type = *src++;
+	
+	while ( n-- > 0 )
+	{
+		switch ( *src )
+		{
+		case FEND:
+			return EBADMSG;
+			break;
+		
+		case FESC:
+			if ( n-- == 0 )
+				return EBADMSG;
+			
+			++src;
+			switch ( *src )
+			{
+			case TFEND:
+				*dst++ = FEND;
+				break;
+			
+			case TFESC:
+				*dst++ = FESC;
+				break;
+				
+			default:
+				return EBADMSG;
+			}
+			break;
+		
+		default:
+			*dst++ = *src;
+		}
+		
+		++src;
+	}
+	
+	if ( *src != FEND )
+		return EBADMSG;
+	
+	return 0;
+}
+
 void kiss_send( ax_iface *self, void *frame, size_t len)
 {
+	kiss_iface_attrs *attrs = (kiss_iface_attrs*) self->p;
+	size_t buffer_len = kiss_encoded_size( frame, len);
+	unsigned char *buffer = (unsigned char *) malloc( buffer_len);
+	if ( buffer == NULL )
+		return; // FIXME: more robust OOM handling
+	
+	kiss_encode( frame, len, 0x00U, buffer);
+	send( attrs->client_fd, buffer, buffer_len, 0);
+	
+	free( buffer);
 }
 
 void *kiss_client_handler( void* arg)
@@ -85,7 +240,7 @@ void *kiss_client_handler( void* arg)
 	
 	while (1) {
 		size_t bytes_read = recv(attrs->client_fd, buffer, BUFFER_SIZE - 1, 0);
-		if (bytes_read <= 0) {
+		if (bytes_read == 0) {
 			break;
 		}
 
@@ -96,7 +251,11 @@ void *kiss_client_handler( void* arg)
 	}
 	
 	printf("Client disconnected.\n");
-	self->ifdown( self);
+	
+	// Ask the DSE to shut the interface
+	if ( attrs->sw != NULL && attrs->sw->close_iface != NULL )
+		attrs->sw->close_iface( attrs->sw, self->id);
+	
 	while(1) {
 		sleep(1);
 	}
